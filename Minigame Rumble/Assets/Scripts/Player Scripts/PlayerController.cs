b71fa8fc602dad71205afd40b820477d57ca7bb0
Matthew-Maragos <m_maragos@u.pacific.edu>
2025-04-09 19:52:16 -0700

    A player character has been added. This character is able to jump with the press of the A button and moves only to the right. Also the movement to the right is animated.

---
+++ Minigame Rumble/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerControls controls;
+    private Rigidbody2D rb;
+    private SPUM_Prefabs spum;
+
+    [Header("Settings")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius = 0.2f;
+
+    private bool isGrounded;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Jumping.Jump.performed += ctx => Jump();
+    }
+
+    void OnEnable() => controls.Enable();
+    void OnDisable() => controls.Disable();
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spum = GetComponent<SPUM_Prefabs>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Always moving to the right
+        rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+    
+    
+}
