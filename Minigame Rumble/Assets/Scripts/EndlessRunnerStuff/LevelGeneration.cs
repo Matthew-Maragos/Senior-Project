d4caaa682f24ab63478246e78dfe8aab33498bc9
SagarMathur <s_mathur4@u.pacific.edu>
2025-04-23 19:31:53 -0700

    Fixed some levels up and modified the logic for spawning

--- Minigame Rumble/Assets/Scripts/EndlessRunnerStuff/LevelGeneration.cs
+++ Minigame Rumble/Assets/Scripts/EndlessRunnerStuff/LevelGeneration.cs
@@ -22,13 +22,13 @@ public class LevelGeneration : MonoBehaviour
         GameObject firstLevel = Instantiate(levelToSpawnFirst, new Vector3(0, spawnY, 0), Quaternion.identity);
         levelHeiarchy.Add(firstLevel);
         
-        //Measure its' width for spacing
+        //Measure its width for spacing
         SpriteRenderer sr = firstLevel.GetComponent<SpriteRenderer>();
-        tileWidth = (sr != null) ? sr.sprite.bounds.size.x : 20f;
+        tileWidth = (sr != null) ? sr.sprite.bounds.size.x : 10f;
         
         //Compute where the next level should appear
-        nextSpawnX = (firstLevel.transform.position.x + tileWidth) + 55f;
-        
+        nextSpawnX = (firstLevel.transform.position.x + tileWidth);
+       //nextSpawnX = -20f;
         StartCoroutine(SpawnLoop());
     }
 
@@ -41,41 +41,6 @@ public class LevelGeneration : MonoBehaviour
     void SpawnLevel()
     {
         GameObject prefab = levelObjects[Random.Range(0, levelObjects.Count)];
-        
-        //Reposition the levels for different spawnings
-        switch (prefab.name)
-        {
-            case "FirstLevel":
-                spawnY = -2.5f;
-                break;
-            case "SecondLevel":
-                spawnY = 6.42f;
-                break;
-            case "ThirdLevel":
-                spawnY = 2.7f;
-                break;
-            case "FourthLevel":
-                spawnY = 2.18f;
-                break;
-            case "FifthLevel 1":
-                spawnY = 0.71f;
-                break;
-            case "FifthLevel2":
-                spawnY = 0.71f;
-                break;
-            case "FifthLevel3":
-                spawnY = 0.71f;
-                break;
-            case "FifthLevel4":
-                spawnY = 0.71f;
-                break;
-            case "FifthLevel5":
-                spawnY = 0.71f;
-                break;
-            case "FifthLevel6":
-                spawnY = 0.71f;
-                break;
-        }
         GameObject inst = Instantiate(prefab, new Vector3(nextSpawnX, spawnY, 0), Quaternion.identity);
         
         levelHeiarchy.Add(inst);
