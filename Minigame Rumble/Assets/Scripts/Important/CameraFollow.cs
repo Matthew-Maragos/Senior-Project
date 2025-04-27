3b4a69840590323ceef19e77c1eef273a0b0498e
Matthew-Maragos <m_maragos@u.pacific.edu>
2025-04-26 23:22:08 -0700

    All levels have a background.  Added in couple more prefabs to do this but now the game looks good with a pretty nice background to go with it.

--- Minigame Rumble/Assets/Scripts/Important/CameraFollow.cs
+++ Minigame Rumble/Assets/Scripts/Important/CameraFollow.cs
@@ -1,29 +1,54 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CameraFollow : MonoBehaviour
-{ 
-    public Transform player;
-
-    private Vector3 lastPlayerPosition;
+{
+    public List<Transform> players = new List<Transform>(); // List of all players
+    public float cameraSpeed = 5f; // Speed at which camera moves when no one is pushing it
+    private Vector3 lastCameraPosition;
 
     void Start()
     {
-        if (player != null)
-        {
-            lastPlayerPosition = player.position;
-        }
+        lastCameraPosition = transform.position;
     }
 
     void LateUpdate()
     {
-        if (player != null)
+        // Remove any players that have been destroyed (null entries)
+        players.RemoveAll(player => player == null);
+
+        if (players.Count > 0)
         {
-            float deltaX = player.position.x - lastPlayerPosition.x;
+            // Find the furthest X position of all alive players
+            float furthestPlayerX = float.MinValue;
+            foreach (Transform player in players)
+            {
+                if (player.position.x > furthestPlayerX)
+                {
+                    furthestPlayerX = player.position.x;
+                }
+            }
 
-            // Move the camera only on the X axis by the same amount the player moved
-            transform.position += new Vector3(deltaX, 0f, 0f);
+            // Move the camera toward the furthest player, or at a steady speed
+            float deltaX = furthestPlayerX - lastCameraPosition.x;
 
-            lastPlayerPosition = player.position;
+            // If players are still pushing the camera forward
+            if (deltaX > 0f)
+            {
+                transform.position += new Vector3(deltaX, 0f, 0f);
+            }
+            else
+            {
+                // Move at steady speed if no players are pushing forward
+                transform.position += new Vector3(cameraSpeed * Time.deltaTime, 0f, 0f);
+            }
+
+            lastCameraPosition = transform.position;
+        }
+        else
+        {
+            // No players left alive -> Stop moving (or you can trigger a game over here)
+            Debug.Log("All players are dead. Camera stopped.");
         }
     }
 }
